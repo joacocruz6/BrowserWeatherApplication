435cfc7e5b9a93c08f2cdacfea62a9c36612942e
Joaquin Cruz D <joacocruz6@gmail.com>
2019-10-17 00:00:55 -0300

    Change the view and html to accept the form

--- weather/views.py
+++ weather/views.py
@@ -1,21 +1,29 @@
 import requests
 from django.shortcuts import render
 from .models import City
+from .forms import CityForm
 # Create your views here.
 def index(request):
-    cities = City.object.all()
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        form.save()
+    
+    form = CityForm()
+    cities = City.objects.all()
     api_url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial?&appid=e010758d5c27c27970314c9a74fa6d04'
     weather_data = list()
     for city in cities:
-        response = request.get(api_url.format(city))
+        response = requests.get(api_url.format(city)).json()
+        print(response['main'])
         city_weather = {
-        'city': city,
-        'temperature' : response['main']['temp'],
-        'description' : response['weather'][0]['description'],
-        'icon' : response['weather'][0]['icon'],
+            'city': city.name,
+            'temperature' : response['main']['temp'],
+            'description' : response['weather'][0]['description'],
+            'icon' : response['weather'][0]['icon'],
         }
         weather_data.append(city_weather)
     context = { 
         'weather_data' : weather_data,
+        'form' : form,
          }
     return render(request,'weather/weather.html',context)
