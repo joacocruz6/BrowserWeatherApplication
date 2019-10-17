8e4bb45506142eca1b816c745f0a217199f1e296
Joaquin Cruz D <joacocruz6@gmail.com>
2019-10-16 23:32:45 -0300

    Updated the view to query for the cities

--- weather/views.py
+++ weather/views.py
@@ -1,18 +1,21 @@
 import requests
 from django.shortcuts import render
-
+from .models import City
 # Create your views here.
 def index(request):
-    city = 'Región Metropolitana de Santiago'
-    api_url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&units=imperial?&appid=e010758d5c27c27970314c9a74fa6d04'
-    response = requests.get(api_url).json()
-    print(response)
-    city_weather = {
+    cities = City.object.all()
+    api_url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial?&appid=e010758d5c27c27970314c9a74fa6d04'
+    weather_data = list()
+    for city in cities:
+        response = request.get(api_url.format(city))
+        city_weather = {
         'city': city,
         'temperature' : response['main']['temp'],
         'description' : response['weather'][0]['description'],
         'icon' : response['weather'][0]['icon'],
-    }
-    
-    context = { 'city_weather' : city_weather }
+        }
+        weather_data.append(city_weather)
+    context = { 
+        'weather_data' : weather_data,
+         }
     return render(request,'weather/weather.html',context)
