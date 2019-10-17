db86e374b08a93998bbdab11d53b0d35a600ee0a
Joaquin Cruz D <joacocruz6@gmail.com>
2019-10-16 23:11:33 -0300

    Created a view with the weather API

--- weather/views.py
+++ weather/views.py
@@ -1,5 +1,18 @@
+import requests
 from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+    city = 'Santiago'
+    api_url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric?&appid=e010758d5c27c27970314c9a74fa6d04'
+    response = requests.get(api_url).json()
+    print(response)
+    city_weather = {
+        'city': city,
+        'temperature' : response['main']['temp'],
+        'description' : response['weather'][0]['description'],
+        'icon' : response['weather'][0]['icon'],
+    }
+    
+    context = {'city_weather' : city_weather }
     return render(request,'weather/weather.html')
